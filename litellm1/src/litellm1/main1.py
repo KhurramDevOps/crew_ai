4fb5fcd982954c6040ead624fb41d17017bbd215
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-02-26 20:42:41 +0500

    litellm practice with crewai

---
+++ litellm1/src/litellm1/main1.py
@@ -0,0 +1,23 @@
+from crewai.flow import Flow, listen , start
+from litellm import completion
+
+class LitellmFlow(Flow):
+
+    @start()
+    def start_function(self):
+        output = completion(
+            model="gemini/gemini-2.0-flash",
+            messages=[{
+                'role': 'user',
+                'content': 'Where blue ringed octopus is found?'
+            }]
+        )
+        return output
+
+def run_litellm_flow():
+    obj = LitellmFlow()
+    result = obj.kickoff()
+    print(result)
+
+
+
